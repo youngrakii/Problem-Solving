3ff72eddeef1630fd305fa1d515e467e5b658273
정영락 <117638540+youngrakii@users.noreply.github.com>
2024-05-22 01:07:27 +0900

    [Bronze I] Title: 피보나치 수 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.cc
+++ 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.cc
@@ -1,14 +1,16 @@
 #include <iostream>
 using namespace std;
-int main(){
-    int n;
-    cin >> n;
-    long long dp[91]; // 범위 초과 방지
-    dp[0] = 0;
-    dp[1] = 1;
-    for (int i = 2; i <= n; i++){
-        dp[i] = dp[i - 1] + dp[i - 2]; // 그 전 값 + 그 전전 값
-    }
-    cout << dp[n];
-    return 0;
-} // dynamic programing
+long long fiboarr[100] = {0,1,};
+long long fibo(int N)
+{
+    if(N == 0 || N == 1)
+        return fiboarr[N];
+    else if(fiboarr[N] == 0)
+        fiboarr[N] = fibo(N-1) + fibo(N-2);
+    return fiboarr[N];
+}
+int main() {
+    int N;
+    cin >> N;
+    cout << fibo(N);
+}
