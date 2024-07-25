cac87bf81498d37aa7eed4ccbf681c8f4b85f1ab
정영락 <117638540+youngrakii@users.noreply.github.com>
2024-07-25 22:32:34 +0900

    [Silver I] Title: RGB거리, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.cc
+++ 백준/Silver/1149. RGB거리/RGB거리.cc
@@ -4,29 +4,29 @@
 
 using namespace std;
 
-int main() {
-    int N;
-    cin >> N;
-
-    vector<vector<int>> cost(N, vector<int>(3));
-    vector<vector<int>> dp(N, vector<int>(3));
-
-    for (int i = 0; i < N; ++i) {
-        cin >> cost[i][0] >> cost[i][1] >> cost[i][2];
+int main(){
+    int n;
+    cin>>n;
+    
+    vector<vector<int>> cost(n,vector<int>(3));
+    vector<vector<int>> dp(n,vector<int>(3));
+    
+    for(int i=0;i<n;i++){
+        cin>>cost[i][0]>>cost[i][1]>>cost[i][2];
     }
-
-    dp[0][0] = cost[0][0];
-    dp[0][1] = cost[0][1];
-    dp[0][2] = cost[0][2];
-
-    for (int i = 1; i < N; ++i) {
-        dp[i][0] = min(dp[i-1][1], dp[i-1][2]) + cost[i][0];
-        dp[i][1] = min(dp[i-1][0], dp[i-1][2]) + cost[i][1];
-        dp[i][2] = min(dp[i-1][0], dp[i-1][1]) + cost[i][2];
+    
+    dp[0][0]=cost[0][0];
+    dp[0][1]=cost[0][1];
+    dp[0][2]=cost[0][2];
+    
+    for(int i=1;i<n;i++){
+        dp[i][0]=min(dp[i-1][1],dp[i-1][2])+cost[i][0];
+        dp[i][1]=min(dp[i-1][0],dp[i-1][2])+cost[i][1];
+        dp[i][2]=min(dp[i-1][0],dp[i-1][1])+cost[i][2];
     }
-
-    int result = min({dp[N-1][0], dp[N-1][1], dp[N-1][2]});
-    cout << result << endl;
-
+    
+    int result = min({dp[n-1][0],dp[n-1][1],dp[n-1][2]});
+    cout<<result<<"\n";
+    
     return 0;
 }
