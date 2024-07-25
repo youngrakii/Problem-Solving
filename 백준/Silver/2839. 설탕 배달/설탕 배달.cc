0e3ff2bd1c5a710208cd546aff7270e4a69dbbd5
정영락 <117638540+youngrakii@users.noreply.github.com>
2024-07-25 22:19:00 +0900

    [Silver IV] Title: 설탕 배달, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.cc
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.cc
@@ -3,22 +3,16 @@ using namespace std;
 
 int main(){
     int n;
-    int dp[5001];
+    int bag=0;
     cin>>n;
-    dp[3]=1;
-    dp[5]=1;
-    for(int i=6;i<=n;i++){
-        if(dp[i-3]){
-            dp[i]=dp[i-3]+1;
+    while(n>=0){
+        if(n%5==0){
+            bag+=n/5;
+            cout<<bag<<"\n";
+            return 0;
         }
-        if(dp[i-5]){
-            dp[i]=dp[i-5]+1;
-        }
+        n-=3;
+        bag++;
     }
-    if(dp[n]){
-        cout<<dp[n];
-    }
-    else{
-        cout<<-1;
-    }
+    cout<<-1<<"\n";
 }
