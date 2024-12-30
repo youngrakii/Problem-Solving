cb864becc644eafa5b3341a766b7031ac85a6eea
정영락 <117638540+youngrakii@users.noreply.github.com>
2024-12-31 02:45:46 +0900

    [Silver II] Title: 특정 거리의 도시 찾기, Time: 284 ms, Memory: 22400 KB -BaekjoonHub

--- 백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.cc
+++ 백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.cc
@@ -1,71 +1,69 @@
-#define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
-#include <cstdio>
-#include <algorithm>
 #include <vector>
-#include <cstring>
-#include <string>
 #include <queue>
-#include <stack>
-#include <cmath>
-#define INF 987654321
 
 using namespace std;
 
-queue<pair<int, int>> q;
-vector<pair<int, int>> line[300001];
-int check[300001];
-int Distance[300001];
-int N, M, K, start;
+int N, M, K, X;
 
-void bfs(int x) {
-	Distance[x] = 0;
-	q.push({ 0,x });
+bool visited[300001]; //노드 방문 여부
+int dist[300001];     // 출발노드에서 각 노드에 걸리는 거리
+vector<int> graph[300001];
+
+//BFS 함수 정의
+void bfs(int start) {
+	queue<pair<int, int>> q;
+	q.push({ start,0 });
+
+	//현재 노드를 방문 처리
+	visited[start] = true;
+
+	//큐가 빌 때까지 반복
 	while (!q.empty()) {
-		int X = q.front().second;
-		int cost = q.front().first;
-		check[X] = 1;
+		//큐에서 하나의 원소를 뽑아 출력
+		int x = q.front().first;
+		int cnt = q.front().second+1;
 		q.pop();
-		for (int i = 0; i < line[X].size(); i++) {
-			int xx = line[X][i].first;
-			int Cost = line[X][i].second;
-			if (check[xx] == 0) {
-				if (Distance[xx] > Distance[X] + Cost) {
-					Distance[xx] = Distance[X] + Cost;
-					q.push({ Distance[xx],xx });
-				}
+		//해당 원소와 연결된, 아직 방문하지 않은 원소들을 큐에 삽입
+		for (int i = 0; i < graph[x].size(); i++) {
+			int y = graph[x][i];
+			if (visited[y] == false) {
+				dist[y] = cnt;
+				visited[y] = true;
+				q.push({ y,cnt });
 			}
 		}
 	}
 }
 
-void solve() {
-	vector<int> point;
-	for (int i = 1; i <= N; i++) Distance[i] = INF;
-	bfs(start);
-	for (int i = 1; i <= N; i++) {
-		if (Distance[i] == K) {
-			point.push_back(i);
-		}
-	}
-	if (point.size() == 0) cout << "-1";
-	else {
-		sort(point.begin(), point.end());
-		for (int i = 0; i < point.size(); i++) {
-			cout << point[i] << "\n";
-		}
-	}
-}
-
 int main() {
+	//입출력 빠르게 해주는 코드
+	ios_base::sync_with_stdio(0);
 	cin.tie(0);
 	cout.tie(0);
-	cin >> N >> M >> K >> start;
+
+	//입력
+	cin >> N >> M >> K >> X;
+
+	//그래프 노드, 간선 연결 입력
 	for (int i = 0; i < M; i++) {
 		int x, y;
 		cin >> x >> y;
-		line[x].push_back({ y,1 });
+		graph[x].push_back(y);
 	}
-	solve();
-	return 0;
+
+	//bfs 호출
+	bfs(X);
+
+	//거리가 K인 노드 출력, 출력하지 않으면 -1 출력
+	bool check = false;
+	for (int i = 1; i <= N; i++) {
+		if (dist[i] == K) {
+			check = true;
+			cout << i << "\n";
+		}
+	}
+	if (check == false) {
+		cout << -1 << "\n";
+	}
 }
