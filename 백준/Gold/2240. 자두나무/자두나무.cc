d3d49501efd17aaea6ca2b9683624c8244586b40
정영락 <117638540+youngrakii@users.noreply.github.com>
2025-02-12 15:58:12 +0900

    [Gold V] Title: 자두나무, Time: 0 ms, Memory: 2268 KB -BaekjoonHub

--- 백준/Gold/2240. 자두나무/자두나무.cc
+++ 백준/Gold/2240. 자두나무/자두나무.cc
@@ -2,53 +2,53 @@
 #include <algorithm>
 
 using namespace std;
-/*
- * dp[Tree][Time][Move]
- */
+int T, W;
 
-int dp[3][1001][32];
-int tree[1001];
+int list[1001];
+int dp[31][2][1001];
+// dp[이동횟수][현재위치][시간]
 
-int main() {
-    
-    ios::sync_with_stdio(0);
-    cin.tie(0);
+int main()
+{
+	std::ios::sync_with_stdio(false);
+	std::cin.tie(NULL);
 
-    int T, W;
+	// 입력
+	cin >> T >> W;
+	for (int i = 1; i <= T; i++)
+		cin >> list[i];
 
-    cin >> T >> W;
-    
-    for (int i = 1; i <= T; ++i) {
-        cin >> tree[i];
-    }
 
-    if (tree[1] == 1) {
-        dp[1][1][1] = 1;
-    } else {
-        dp[2][1][2] = 1;
-    }
-    
-    for (int i = 2; i <= T; ++i) {
-        
-        for (int mv = 1;  mv <= W + 1; ++mv) {
-            
-            if (tree[i] == 1) {
-                dp[1][i][mv] = max(dp[1][i - 1][mv], dp[2][i - 1][mv - 1]) + 1;
-                dp[2][i][mv] = max(dp[1][i - 1][mv - 1], dp[2][i - 1][mv]);
-            } 
-            else {
-                dp[1][i][mv] = max(dp[1][i - 1][mv], dp[2][i - 1][mv - 1]);
-                dp[2][i][mv] = max(dp[1][i - 1][mv - 1], dp[2][i - 1][mv]) + 1;
-            }
-            
-        }
-    }
+	for (int j = 0; j <= W; j++)
+	{
+		for (int i = 1; i <= T; i++)
+		{
+			if (j == 0)
+			{	//처음 시작시 자두는 1번 나무 아래 위치해있다.
+				dp[j][0][i] = dp[j][0][i - 1] + (list[i] == 1);
+			}
+			else
+			{
+				//첫 시작 이후는, 위치 움직임or안움직임, 받음or못받음을 확인하며 체크한다.
+				dp[j][0][i] = max(dp[j][0][i - 1] + (list[i] == 1), dp[j - 1][1][i - 1] + (list[i] == 1));
+				dp[j][1][i] = max(dp[j][1][i - 1] + (list[i] == 2), dp[j - 1][0][i - 1] + (list[i] == 2));
+				// dp[이동횟수][자두의 위치][흐른 시간] = 
+				// (이동하지않고 시간만 1초 흐름 + i번째 입력과 현재 위치가 같은가?), 
+				// (이동했고, 1초 흐름 + i번째 입력과 현재 위치가 같은가?) 중 큰 값
+			}
+		}
+	}
 
-    int res = -1;
-    
-    for (int i = 1; i <= W + 1; ++i) {
-        res = max({res, dp[1][T][i], dp[2][T][i]});
-    }
-    
-    cout << res;
+
+	int ans = 0;
+	for (int i = 0; i < 2; i++)
+	{
+		for (int j = 0; j <= W; j++)
+		{
+        	// 마지막에 가능한 모든 이동횟수와, 위치중 가장 큰 값을 출력한다.
+			ans = max(ans, dp[j][i][T]);
+		}
+	}
+
+	cout << ans << '\n';
 }
