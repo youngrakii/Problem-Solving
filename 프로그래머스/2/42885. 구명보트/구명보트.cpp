0ddffb764dca1cf18df3284e5361b285beddf792
정영락 <117638540+youngrakii@users.noreply.github.com>
2025-02-28 02:06:56 +0900

    [level 2] Title: 구명보트, Time: 1.50 ms, Memory: 4.74 MB -BaekjoonHub

---
+++ 프로그래머스/2/42885. 구명보트/구명보트.cpp
@@ -0,0 +1,25 @@
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int solution(vector<int> people, int limit){
+    int answer=0;
+    int i=0;
+    
+    sort(people.begin(), people.end());
+    
+    while(people.size()>i){
+        if(people[i]+people.back()<=limit){
+            answer++;
+            i++;
+            people.pop_back();
+        }
+        else{
+            answer++;
+            people.pop_back();
+        }
+    }
+    
+    return answer;
+}
