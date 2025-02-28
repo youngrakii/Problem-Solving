a902f6fad3fae1ea51cc2bf23f3323d38a06b2ea
정영락 <117638540+youngrakii@users.noreply.github.com>
2025-02-28 23:12:56 +0900

    [Silver III] Title: 1로 만들기, Time: 4 ms, Memory: 5928 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.cc
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.cc
@@ -5,6 +5,10 @@
 using namespace std;
 
 int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    
     int n;
     cin>>n;
     vector<int> dp(n+1);
@@ -12,13 +16,8 @@ int main(){
     dp[1]=0;
     
     for(int i=2; i<=n; i++){
-        //1. 1을 뺀다.
         dp[i]=dp[i-1]+1;
-        
-        //2. 3으로 나누어지면
         if(!(i%3)) dp[i] = min(dp[i],dp[i/3]+1);
-        
-        //3. 2로 나누어지면
         if(!(i%2)) dp[i] = min(dp[i], dp[i/2]+1);
     }
     
