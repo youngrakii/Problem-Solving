6c8a02b4b879d72f945ee49273b2b0b96e69a05c
정영락 <117638540+youngrakii@users.noreply.github.com>
2025-03-07 12:30:59 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 0.25 ms, Memory: 4.13 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.cpp
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.cpp
@@ -1,36 +1,38 @@
-#include <vector>
-#include <queue>
-#include <iostream>
-
+#include<cstring>
+#include<algorithm>
+#include<queue>
 using namespace std;
 
-int dy[4]={-1,0,1,0};
-int dx[4]={0,1,0,-1};
+bool vst[104][104];
+int dist[104][104];
+int dx[4] = {1,-1,0,0};
+int dy[4] = {0,0,1,-1};
+queue<pair<int,int>> q;
 
-int BFS(vector<vector<int>>& maps){
-    queue<pair<int,int>> q;
-    int n=maps.size();
-    int m=maps[0].size();
-    vector<vector<int>> table(n,vector<int>(m,0));
+int solution(vector<vector<int>> m)
+{
+    int ans = 0;
+    int ns = m.size();
+    int ms = m[0].size();
+    memset(dist, -1, sizeof(dist));
     q.push({0,0});
-    table[0][0]=1;
-    
-    while(q.size()){
-        pair<int,int> current=q.front();
-        q.pop();
-        for(int i=0; i<4; i++){
-            int ny = current.first+dy[i];
-            int nx = current.second +dx[i];
-            if(ny<0 || nx<0 || ny>=n || nx>=m || maps[ny][nx] == 0 || table[ny][nx]) continue;
-            table[ny][nx] = table[current.first][current.second]+1;
+    vst[0][0] = 1;
+    dist[0][0] = 1;
+    while(!q.empty()){
+        auto cur = q.front(); q.pop();
+        if(cur.first == ns - 1 && cur.second == ms - 1) {
+            return dist[ns-1][ms-1];
+        }
+        for(int dir=0; dir<4; dir++){
+            int nx = cur.second + dx[dir];
+            int ny = cur.first + dy[dir];
+            if(nx < 0 || ny < 0 || nx >= ms || ny >= ns || vst[ny][nx] || m[ny][nx] == 0 || dist[ny][nx] != -1) continue;
             q.push({ny,nx});
+            vst[ny][nx];
+            dist[ny][nx] = dist[cur.first][cur.second] + 1;
         }
     }
-    
-    return table[n-1][m-1]>0 ? table[n-1][m-1]:-1;
-}
-
-int solution(vector<vector<int>> maps){
-    int answer=BFS(maps);
-    return answer;
+    if(dist[ns-1][ms-1] == -1) ans = -1;
+    else ans = dist[ns-1][ms-1];
+    return ans;
 }
