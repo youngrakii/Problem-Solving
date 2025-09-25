0a4d5136c56f7bd023348e0d7d012f14bceb7f9c
정영락 <117638540+youngrakii@users.noreply.github.com>
2025-09-25 20:55:50 +0900

    [Gold III] Title: 파티, Time: 0 ms, Memory: 2340 KB -BaekjoonHub

--- 백준/Gold/1238. 파티/파티.cc
+++ 백준/Gold/1238. 파티/파티.cc
@@ -1,59 +1,71 @@
 #include <iostream>
-#include <queue>
 #include <vector>
-#include <cstring>
-#define INF 98765432
-using namespace std;
-int n,x,m;
-vector<pair<int, int>> v[1002];
-int dst[1002]; // 정점까지의 최단거리를 기록
-int result; // 최단거리
+#include <queue>
  
-// 다익스트라
-void fc(int a, int d){
-    memset(dst, INF, sizeof(dst)); // 초기값 INF
-    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq; 
- // 우선순위 큐(최소 거리가 루트로)
-    pq.push({0, a});// pq(거리, 정점)
-    dst[a] = 0; // 시작 지점 거리 0으로 초기화
-    while(!pq.empty()){
-        int cx = pq.top().second; // 현재 정점
-        int cdst = pq.top().first; // 현재 정점까지 거리
-        pq.pop();
-        if(cx == d){ // 현재 정점에 도착했다면
-            result = dst[cx]; // 최단 거리 기록
-            break;
-        }
-        for (int i = 0; i < v[cx].size();i++){ 
-            // 현재 정점과 이어진 다음 정점들 
-            int nx = v[cx][i].first; // 다음 정점 
-            int ndst = v[cx][i].second + cdst; // 다음 정점까지 거리 계산
-            if(dst[nx] > ndst){
-                 // 다음 정점까지 거리와 기록된 정점까지 거리와 비교해
-                 // 최단거리라면
-                dst[nx] = ndst; // 최단거리 기록
-                pq.push({ndst, nx}); // 큐에 넣어줌
+#define pii pair<int, int>
+ 
+using namespace std;
+ 
+ 
+int N, M, X;
+const int INF = 1e9+7;
+vector<pii > graph[2][1001]; 
+vector<int> dist[2];
+int resdist[1001];
+ 
+void input(){
+    int u, v, t;
+    cin >> N >> M >> X;
+    for(int i = 0; i < M; i++){
+        cin >> u >> v >> t;
+        graph[0][u].push_back(make_pair(t, v));
+        graph[1][v].push_back(make_pair(t, u));
+    }
+    dist[0].resize(N+1, INF);
+    dist[1].resize(N+1, INF);
+}
+ 
+void Dijstra(int k){
+    dist[k][X] = 0;
+    
+    priority_queue<pii, vector<pii >, greater<pii > > que;
+    que.push({0, X});
+    
+    while(!que.empty()){
+        int min_cost = que.top().first;
+        int now = que.top().second;
+        que.pop();
+        
+        if(min_cost > dist[k][now]) continue;
+        
+        for(int i = 0; i < graph[k][now].size(); i++){
+            int next = graph[k][now][i].second;
+            int next_cost = min_cost + graph[k][now][i].first;
+            
+            if(next_cost < dist[k][next]){
+                dist[k][next] = next_cost;
+                que.push({next_cost, next});
             }
         }
     }
 }
+ 
 int main(){
-    cin >> n >> m >> x;
-    int ans1, ans2, ans;
-    ans = 0;
-    int a, b, t;
-    for (int i = 0; i < m;i++){
-        cin >> a >> b >> t;
-        v[a].push_back({b, t});
+    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    input();
+    
+    // 정점들에서 X로 가는 최단거리
+    Dijstra(1);
+    
+    // X에서 정점들로 가는 최단거리
+    Dijstra(0);
+    
+    int res = 0;
+    for(int i = 1; i <= N; i++){
+        res = max(res, dist[0][i] + dist[1][i]);
     }
-    for (int i = 1; i <= n;i++){
-        fc(i, x); // 가는 길 최단 거리 
-        ans1 = result; 
-        fc(x, i); // 오는 길 최단 거리
-        ans2 = result;
-        if(ans < ans1+ans2)
-            ans = ans1 + ans2; // 합
-    }
-    cout << ans << '\n'; // 최단 거리 출력
+    
+    cout << res;
+    
     return 0;
 }
