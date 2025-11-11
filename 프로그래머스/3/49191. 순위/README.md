10b6e6c5addcd947077577af66accd9dc0181024
정영락 <117638540+youngrakii@users.noreply.github.com>
2025-11-11 14:40:56 +0900

    [level 3] Title: 순위, Time: 1.46 ms, Memory: 4.1 MB -BaekjoonHub

--- 프로그래머스/3/49191. 순위/README.md
+++ 프로그래머스/3/49191. 순위/README.md
@@ -1,10 +1,10 @@
 # [level 3] 순위 - 49191 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/49191?language=cpp) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/49191) 
 
 ### 성능 요약
 
-메모리: 4.21 MB, 시간: 1.48 ms
+메모리: 4.1 MB, 시간: 1.46 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 02월 26일 16:12:16
+2025년 11월 11일 14:40:53
 
 ### 문제 설명
 
--- 프로그래머스/3/49191. 순위/순위.cpp
+++ 프로그래머스/3/49191. 순위/순위.cpp
@@ -3,27 +3,35 @@
 
 using namespace std;
 
-int solution(int n, vector<vector<int>> results){
-    int answer=0;
-    vector<vector<bool>> graph(n+1, vector<bool>(n+1,false));
-    //승리한 경우 추가
-    for(auto r:results) graph[r[0]][r[1]]=true;
-    for(size_t i=1; i<=n;i++){
-        for(size_t j=1; j<=n; j++){
-            for(size_t k=1; k<=n; k++){
-                if(graph[j][i]&&graph[i][k]){
-                    graph[j][k]=true;
+int solution(int n, vector<vector<int>> results) {
+    int answer = 0;
+    bool A[101][101]={false,};
+    
+    for(auto r:results){
+        A[r[0]][r[1]] = true;
+    }
+    
+    for(int k=1; k<=n;k++){
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=n; j++){
+                if(A[i][k]==true && A[k][j] == true){
+                    A[i][j]=true;
                 }
             }
         }
     }
     
-    for(size_t i=1; i<=n; i++){
-        int count=0;
-        for(size_t j=1; j<=n; j++){
-            if(graph[i][j]||graph[j][i]) count++;
+    for(int a=1; a<=n; a++){
+        bool flag = true;
+        for(int b=1; b<=n; b++){
+            if(a==b) continue;
+            if(A[a][b]==false && A[b][a]==false){
+                flag = false;
+                break;
+            }
         }
-        if(count==n-1) answer++;
+        
+        if(flag == true) answer++;
     }
     return answer;
 }
