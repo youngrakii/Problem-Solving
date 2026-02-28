3037bc85a270e4b322982e7dc76f0484ff0ad3c3
정영락 <117638540+youngrakii@users.noreply.github.com>
2026-02-28 13:44:15 +0900

    [level 2] Title: 의상, Time: 0.03 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/README.md
+++ 프로그래머스/2/42578. 의상/README.md
@@ -1,10 +1,10 @@
 # [level 2] 의상 - 42578 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42578?language=cpp) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42578) 
 
 ### 성능 요약
 
-메모리: 4.19 MB, 시간: 0.03 ms
+메모리: 4.2 MB, 시간: 0.03 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 02월 27일 17:02:33
+2026년 02월 28일 13:44:12
 
 ### 문제 설명
 
--- 프로그래머스/2/42578. 의상/의상.cpp
+++ 프로그래머스/2/42578. 의상/의상.cpp
@@ -4,16 +4,23 @@
 
 using namespace std;
 
-int solution(vector<vector<string>> clothes){
-    int answer=1;
-    unordered_map<string, int> m;
-    for(auto p : clothes){
-        m[p[1]]++;
+int solution(vector<vector<string>> clothes) {
+    int answer = 0; // 최종 조합 수를 저장할 변수
+    
+    unordered_map<string, vector<string>> m; // 의상 종류별로 의상을 저장할 맵
+    
+    // 의상을 종류별로 그룹화합니다.
+    for(auto v : clothes)
+    {
+        m[v[1]].push_back(v[0]); // 종류를 키로, 의상 이름을 값으로 추가
     }
     
-    for(auto p:m){
-        answer*=(p.second+=1);
+    int temp = 1; // 조합 수를 계산하기 위한 변수
+    // 각 종류별로 가능한 조합 수를 계산합니다.
+    for(auto p : m)
+    {
+        temp *= p.second.size() + 1; // 각 의상 종류에서 착용하지 않는 경우를 포함하여 곱합니다.
     }
     
-    return answer-1;
+    return temp - 1; // 최소 한 개의 의상은 착용하므로 1을 빼줍니다.
 }
