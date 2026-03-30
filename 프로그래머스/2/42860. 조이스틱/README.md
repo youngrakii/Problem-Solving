4efd3bfdda0f27077d882f460019f101dc83b149
정영락 <117638540+youngrakii@users.noreply.github.com>
2026-03-30 23:26:38 +0900

    [level 2] Title: 조이스틱, Time: 0.01 ms, Memory: 4.15 MB -BaekjoonHub

--- 프로그래머스/2/42860. 조이스틱/README.md
+++ 프로그래머스/2/42860. 조이스틱/README.md
@@ -1,10 +1,10 @@
 # [level 2] 조이스틱 - 42860 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42860?language=cpp) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42860) 
 
 ### 성능 요약
 
-메모리: 3.64 MB, 시간: 0.01 ms
+메모리: 4.15 MB, 시간: 0.01 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 03월 05일 20:38:42
+2026년 03월 30일 23:26:34
 
 ### 문제 설명
 
--- 프로그래머스/2/42860. 조이스틱/조이스틱.cpp
+++ 프로그래머스/2/42860. 조이스틱/조이스틱.cpp
@@ -1,30 +1,38 @@
 #include <string>
 #include <vector>
-#include <iostream>
 #include <algorithm>
 
-#define min(a,b) ((a) > (b) ? (b) : (a))
 using namespace std;
 
-int solution(string name) 
-{
+int solution(string name) {
     int answer = 0;
-    int L2 = 0;
-    int move = name.size() - 1;
-    int length = name.size();
+    int n = name.size();
 
-    for(int L1=0; L1<name.size();L1++) // L1 == i
-    {   
-        answer += min(name[L1] - 'A','Z' - name[L1] +1);        
-        
-        int L2_idx = L1+1;
-        while(L2_idx < length)
-        {
-            if(name[L2_idx] == 'A') L2_idx++;
-            else break;
-        }
-        L2 = length - 1 - L2_idx + 1;
-        move = min(move, L1 + L2 + min(L1, L2));         
+    // 1. 위아래 이동 횟수 계산
+    for (char c : name) {
+        answer += min(c - 'A', 'Z' - c + 1);
     }
-    return answer + move;
+
+    // 2. 좌우 이동 횟수 계산
+    int move = n - 1;  // 기본: 오른쪽으로만 끝까지 이동
+
+    for (int i = 0; i < n; i++) {
+        int next = i + 1;
+
+        // i 다음부터 연속된 A 구간 찾기
+        while (name[next] == 'A') {
+            next++;
+        }
+
+        // 오른쪽으로 갔다가 돌아오기
+        int case1 = i * 2 + (n - next);
+
+        // 왼쪽(끝쪽) 먼저 갔다가 오기
+        int case2 = i + (n - next) * 2;
+
+        move = min(move, min(case1, case2));
+    }
+
+    answer += move;
+    return answer;
 }
