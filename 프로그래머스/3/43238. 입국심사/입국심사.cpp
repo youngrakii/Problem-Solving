76b5951a5f3aef0e557c11cae1d3653a44776216
정영락 <117638540+youngrakii@users.noreply.github.com>
2026-04-09 15:24:55 +0900

    [level 3] Title: 입국심사, Time: 50.04 ms, Memory: 6.8 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/입국심사.cpp
+++ 프로그래머스/3/43238. 입국심사/입국심사.cpp
@@ -7,29 +7,25 @@ using namespace std;
 long long solution(int n, vector<int> times) {
     long long answer = 0;
     
-    sort(times.begin(), times.end());
+    sort(times.begin(),times.end());
+    long long left =1;
+    long long right = (long long)times.back()*n;
     
-    long long left = 1;
-    long long right = (long long)times.back() * n;
-    
-    while (left <= right) {
-        long long mid = (left + right) / 2;
+    while(left<=right){
+        long long mid = (left+right)/2;
+        long long sum=0;
         
-        long long people = 0;
-        for (int t : times) {
-            people += mid / t;
-            
-            // 오버플로우 방지 + 더 셀 필요 없을 때
-            if (people >= n) break;
+        for(long long time:times){
+            sum+=mid/time;
+            //if(sum>=n) break;
         }
         
-        if (people >= n) {
+        if(sum>=n){
             answer = mid;
-            right = mid - 1;
-        } else {
-            left = mid + 1;
+            right = mid-1;
+        }else{
+            left = mid+1;
         }
     }
-    
     return answer;
 }
