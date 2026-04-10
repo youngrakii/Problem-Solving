5495b7178079cf37685d71bcfd5315f96328a898
정영락 <117638540+youngrakii@users.noreply.github.com>
2026-04-10 19:16:53 +0900

    [level 2] Title: 큰 수 만들기, Time: 4.57 ms, Memory: 5.53 MB -BaekjoonHub

--- 프로그래머스/2/42883. 큰 수 만들기/README.md
+++ 프로그래머스/2/42883. 큰 수 만들기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 큰 수 만들기 - 42883 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42883?language=cpp) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42883) 
 
 ### 성능 요약
 
-메모리: 5.59 MB, 시간: 2523.71 ms
+메모리: 5.53 MB, 시간: 4.57 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 03월 06일 20:11:49
+2026년 04월 10일 19:16:50
 
 ### 문제 설명
 
--- 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.cpp
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.cpp
@@ -3,27 +3,19 @@
 
 using namespace std;
 
-string solution(string number, int k){
-    string answer="";
+string solution(string number, int k) {
+    string result = "";
     
-    int num=number.length()-k; //구해야할 숫자 크기
-    int idx=0;
-    
-    //num 개의 숫자 고르기
-    for(int i=0; i<num; i++){
-        //제일 큰 수 고르기
-        char maxChar = number[idx]; //제일 처음에 있는 수
-        int maxIdx = idx; //인덱스
-        //k개 탐색해서 가장 큰 수 고르기
-        for(int j=idx; j<=k+i; j++){
-            if(maxChar<number[j]){
-                maxChar=number[j];
-                maxIdx=j;
-            }
+    for (char c : number) {
+        while (!result.empty() && k > 0 && result.back() < c) {
+            result.pop_back();
+            k--;
         }
-        answer+=maxChar; //string에 그냥 char +해서 넣을 수 있음.
-        idx=maxIdx+1; // 고른 다음 부분부터 탐색
+        result.push_back(c);
     }
     
-    return answer;
+    // 아직 제거해야 할 개수가 남아 있으면 뒤에서 제거
+    result.resize(result.size()-k);
+    
+    return result;
 }
