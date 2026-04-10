ded0df74bc249271dd628faea0c086af02c845dc
정영락 <117638540+youngrakii@users.noreply.github.com>
2026-04-10 22:58:22 +0900

    [level 3] Title: N으로 표현, Time: 4.10 ms, Memory: 3.74 MB -BaekjoonHub

--- 프로그래머스/3/42895. N으로 표현/N으로 표현.cpp
+++ 프로그래머스/3/42895. N으로 표현/N으로 표현.cpp
@@ -1,54 +1,42 @@
-#include <vector> 
-#include <unordered_set> 
-using namespace std; 
+#include <string>
+#include <vector>
+#include <unordered_set>
 
-int get_Ns(int N, int idx) { 
-    // NN(idx==1), NNN(idx==2), NNNN(idx==3)...과 같은 형태만드는 함수
-    int result = N; 
-    for (int i = 1; i <= idx; i++) { 
-        result = result * 10 + N; 
-    } 
-    return result; 
-} 
+using namespace std;
 
-int solution(int N, int number) { 
-    if (N == number) return 1;  // N과 number가 같다면, N을 한번 사용해서 number를 만들 수 있음
+int solution(int N, int number) {
+    if (N == number) return 1;
     
-    vector< unordered_set<int> > DP(8); 
-    //DP에 저장할 것 -> DP[i] : i개의 N으로 만들 수 있는 숫자들 (set)
+    vector<unordered_set<int>> dp(9); // dp[i] : N을 i번 써서 만들 수 있는 수들
     
-    DP[0].insert(N); // 한개의 N으로 만들 수 있는 수는 N뿐임
-    
-    for (int k = 1; k < 8; k++) { 
+    for (int i = 1; i <= 8; i++) {
+        int repeated = 0;
         
-        // DP[k]에 NNN...(k+1만큼 반복)과 같은 형태 삽입
-        DP[k].insert(get_Ns(N, k));
+        // N, NN, NNN 형태의 수 만들기
+        for (int k = 0; k < i; k++) {
+            repeated = repeated * 10 + N;
+        }
+        dp[i].insert(repeated);
         
-        // DP[k]에 사칙 연산의 결과또한 삽입
-        for (int i = 0; i < k; i++) { 
-            for (int j = 0; j < k; j++) { 
-                if (i + j + 1 != k) continue; 
-                // i+j+1 == k 일때
-                for (int a : DP[i]) { 
-                    for (int b : DP[j]) { 
-                        DP[k].insert(a + b); 
-                        // 검사가 필요한 연산들
-                        
-                        // (1) 음수 존재하면 안됨
-                        if (a - b > 0) 
-                            DP[k].insert(a - b); 
-                        
-                        DP[k].insert(a * b);
-                        
-                        // (2) 0 존재하면 안됨
-                        if (a / b > 0) DP[k].insert(a / b); 
-                    } 
-                } 
-            } 
+        // dp[j]와 dp[i-j]를 조합
+        for (int j = 1; j < i; j++) {
+            for (int a : dp[j]) {
+                for (int b : dp[i - j]) {
+                    dp[i].insert(a + b);
+                    dp[i].insert(a - b);
+                    dp[i].insert(a * b);
+                    if (b != 0) {
+                        dp[i].insert(a / b);
+                    }
+                }
+            }
         }
         
-        if (DP[k].find(number)!=DP[k].end()) //DP set에 number에 해당하는 값이 있으면 k+1을 반환
-            return k + 1; 
-    } 
-    return -1; 
+        // number를 만들 수 있으면 바로 반환
+        if (dp[i].find(number) != dp[i].end()) {
+            return i;
+        }
+    }
+    
+    return -1;
 }
